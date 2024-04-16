e7ec3ee1dcccd250fc902a95f2b2416319bc1f8d
kavvata <gabriel.kawata@gmail.com>
2024-04-15 23:20:44 -0300

    sway, i3blocks and monospace font config

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.9
 
-config.font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" })
+config.font = wezterm.font("GeistMono Nerd Font Mono", { weight = "Regular" })
 config.font_size = 9
 
 return config
