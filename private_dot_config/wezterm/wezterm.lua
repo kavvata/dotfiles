df11bc9107bcd34fa405e51eadd612fd24ca83c1
kavvata <gabriel.kawata@gmail.com>
2024-10-25 00:05:27 -0300

    zenbones colorscheme

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "kanagawabones"
+config.color_scheme = "zenbones_dark"
 
 config.enable_tab_bar = false
--- config.window_background_opacity = 0.98
+-- config.window_background_opacity = 0.80
 
-config.font = wezterm.font("FiraCode Nerd Font")
 -- config.font = wezterm.font_with_fallback({ "Tamzen", "Cozette" })
+config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 9
 
 return config
