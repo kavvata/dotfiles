6d3097effbd0d5486c275898923ddf52fc9e4319
kavvata <gabriel.kawata@gmail.com>
2024-10-27 21:26:05 -0300

    back to tokyonight

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "zenbones_dark"
+config.color_scheme = "kanagawabones"
 
 config.enable_tab_bar = false
--- config.window_background_opacity = 0.80
+-- config.window_background_opacity = 0.98
 
--- config.font = wezterm.font_with_fallback({ "Tamzen", "Cozette" })
 config.font = wezterm.font("FiraCode Nerd Font")
+-- config.font = wezterm.font_with_fallback({ "Tamzen", "Cozette" })
 config.font_size = 9
 
 return config
