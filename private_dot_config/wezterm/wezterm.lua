401d7062f4e90bf350d51aafa01aac1d976a4edf
kavvata <gabriel.kawata@gmail.com>
2024-11-10 20:46:37 -0300

    border radius everywhere!

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "kanagawabones"
 
 config.enable_tab_bar = false
--- config.window_background_opacity = 0.98
+config.window_background_opacity = 0.90
 
 config.font = wezterm.font("FiraCode Nerd Font")
 -- config.font = wezterm.font_with_fallback({ "Tamzen", "Cozette" })
