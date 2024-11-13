f5d38f62f4342b27c0cc73c31af9bff98b4831b4
kavvata <gabriel.kawata@gmail.com>
2024-11-12 22:53:35 -0300

    Fixed packages being installed one by one

--- .setup-script/setup.sh
+++ .setup-script/setup.sh
@@ -4,18 +4,24 @@ if ! command -v git &> /dev/null; then
   sudo pacman -Sy git --noconfirm --needed
 fi
 
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-cd ..
-rm -rf paru
+# git clone https://aur.archlinux.org/paru.git
+# cd paru
+# makepkg -si
+# cd ..
+# rm -rf paru
 
 paru -Syy
+# Absolute path this script is in. /home/user/bin
+SRCPATH=`dirname $(readlink -f $0)`
+
+packages=""
 
 while IFS="" read -r p || [ -n "$p" ]
 do
-  paru -S --needed --noconfirm "$p"
-done < ./packages.txt
+  packages+="$p "
+done < "$SRCPATH/packages.txt"
+
+paru -S --needed --noconfirm $packages
 
 xdg-user-dirs-update
 gsettings set org.gnome.desktop.wm.preferences button-layout ''
@@ -23,4 +29,4 @@ gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 # gsettings set org.gnome.desktop.interface icon-theme Adwaita
 gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
 
-read -p "Don't forget to configure: tuigreeter, keyboard layouts, docker user group and apparmor"
+read -p "Don't forget to configure: tuigreeter, keyboard layouts, waybar components, docker user group and apparmor... (Press any key to finish)"
