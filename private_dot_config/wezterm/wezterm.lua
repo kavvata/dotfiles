4fc70bf234a0c67aa6043351858e512fa7d88a1d
kavvata <gabriel.kawata@gmail.com>
2025-01-29 23:16:29 -0300

    Switching main font from FiraCode NF to JetBrains NF

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.color_scheme = "kanagawabones"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.90
 
-config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 -- config.font = wezterm.font_with_fallback({ "Tamzen", "Cozette" })
 config.font_size = 9
 
