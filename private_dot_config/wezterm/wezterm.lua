926a381b5bd8a80da7cd26adb9cd9615f685484e
kavvata <gabriel.kawata@gmail.com>
2025-05-10 21:40:10 -0300

    feat!: Bringing some stuff from my hyprland rice

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -4,10 +4,12 @@ local config = wezterm.config_builder()
 config.color_scheme = "kanagawabones"
 
 config.enable_tab_bar = false
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.90
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 -- config.font = wezterm.font_with_fallback({ "Tamzen", "Cozette" })
 config.font_size = 9
+config.enable_wayland = true
+config.adjust_window_size_when_changing_font_size = false
 
 return config
