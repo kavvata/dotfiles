f568b7d9dd6e6b0e5b4d30806cb662f3ff19f8b0
kavvata <gabriel.kawata@gmail.com>
2025-05-13 20:59:29 -0300

    feat(setup): added some system-space configurations to the setup-script

--- setup-script/setup.sh
+++ setup-script/setup.sh
@@ -11,7 +11,7 @@ cd ..
 rm -rf paru
 
 paru -Syy
-# Absolute path this script is in. /home/user/bin
+
 SRCPATH=`dirname $(readlink -f $0)`
 
 packages=""
@@ -29,4 +29,29 @@ gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 # gsettings set org.gnome.desktop.interface icon-theme Adwaita
 gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
 
-read -p "Don't forget to configure: tuigreeter, keyboard layouts, waybar components, docker user group and apparmor... (Press any key to finish)"
+echo "Everything is pretty much done. the following steps are for configuring system-space stuff, so you might want to Ctrl-C now." 
+read -p "Arch linux is pretty unstable so these configurations might be out-of-date (Press any key to continue or Ctrl-C to stop)"
+
+read -p "(plymouth) Adding and enabling my custom plymouth theme. (Press any key to continue or Ctrl-C to stop)"
+sudo cp -R ../plymouth-theme/ascii-think /usr/share/plymouth/themes/
+sudo plymouth-set-default-theme -R ascii-think
+
+read -p "(plymouth) Please add plymouth to your hooks (Press any key to open /etc/mkinitcpio.conf or Ctrl-C to stop)"
+sudoedit /etc/mkinitcpio.conf
+
+read -p "(docker) Creating a docker group and adding your user to it. (Press any key to continue or Ctrl-C to stop)"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+
+read -p "(tuigreet) configuring and enabling greetd. (Press any key to continue or Ctrl-C to stop)"
+sudo cp ../systemspace-configs/common/etc/greetd/config.toml /etc/greetd/config.toml
+sudo systemctl enable greetd
+
+
+echo "(apparmor) Please add the following to your boot args:"
+echo "lsm=landlock,lockdown,yama,integrity,apparmor,bpf"
+read -p "Honestly i refuse to touch your boot entry files so i am leaving this up to you (Press any key to continue or Ctrl-C to stop)"
+
+
+read -p "Don't forget to configure: keyboard layouts, waybar components, etc... (Press any key to finish)"
