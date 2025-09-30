b857ae36fd9a0220b6de32927f5bed0655801613
kavvata <gabriel.kawata@gmail.com>
2025-09-29 22:14:40 -0300

    feat(wallpaper): added image preview in kitty

--- private_dot_config/kav-scripts/executable_wallpaper-picker.sh
+++ private_dot_config/kav-scripts/executable_wallpaper-picker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 kitty sh -c 'fd . ~/Pictures/Wallpapers -e jpg -e png -e jpeg -t f \
-  | fzf --bind "enter:execute-silent(ln -sf {} ~/.wallpaper; ln -sf {} ~/.lockscreen; nohup ~/.config/ml4w/scripts/reload-hyprpaper.sh >/dev/null 2>&1 &)+abort"'
+  | fzf --preview-window=top --preview "kitty +kitten icat --stdin=no --clear --transfer-mode=file {}" --bind "enter:execute-silent(ln -sf {} ~/.wallpaper; ln -sf {} ~/.lockscreen; nohup ~/.config/ml4w/scripts/reload-hyprpaper.sh >/dev/null 2>&1 &)+abort"'
