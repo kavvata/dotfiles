4f1904ee131d44305d338998a68e62487f2910af
kavvata <gabriel.kawata@gmail.com>
2026-02-10 22:27:59 -0300

    fix(hyprpaper): fixed shebang

--- private_dot_config/ml4w/scripts/executable_reload-hyprpaper.sh
+++ private_dot_config/ml4w/scripts/executable_reload-hyprpaper.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 killall hyprpaper
 sleep 1
 hyprpaper &
+
--- private_dot_config/niri/executable_reload-bar.sh
+++ private_dot_config/niri/executable_reload-bar.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
-killall waybar
+#!/usr/bin/env bash
+killall .waybar-wrapped
 waybar --config /home/kav/.config/waybar/niri-config.jsonc &
 disown
