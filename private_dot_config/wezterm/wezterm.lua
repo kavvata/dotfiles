d07031ae6457d5f66673257448ddbb7c7023c5a1
kavvata <gabriel.kawata@gmail.com>
2026-03-18 23:02:50 -0300

    feat(wezterm): changed italics from victormono to default iosevkaterm

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -4,21 +4,20 @@ local config = wezterm.config_builder()
 config.color_scheme = "kanagawabones"
 
 config.enable_tab_bar = false
+config.adjust_window_size_when_changing_font_size = false
 
 -- config.font = wezterm.font_with_fallback({ { family = "TX-02 Condensed", weight = "Regular" }, "TX02 Nerd Font Mono" })
 config.font_size = 10
 
-config.font = wezterm.font({ family = "IosevkaTerm Nerd Font", weight = "Medium" })
-
-config.font_rules = {
-	{
-		intensity = "Normal",
-		italic = true,
-		font = wezterm.font({ family = "VictorMono Nerd Font", style = "Italic" }),
-	},
-}
-
+config.font = wezterm.font({ family = "IosevkaTerm Nerd Font", weight = "Regular" })
+-- config.font_rules = {
+-- 	{
+-- 		intensity = "Normal",
+-- 		italic = true,
+-- 		font = wezterm.font({ family = "VictorMono Nerd Font", style = "Italic" }),
+-- 	},
+-- }
+--
 config.enable_wayland = true
-config.adjust_window_size_when_changing_font_size = false
 
 return config
