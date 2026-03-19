e702924c32231c85e387f7157f0a75fe03992522
Gabriel.Kawata - Unespar Campus de Paranaguá <gabriel.kawata@unespar.edu.br>
2026-03-19 07:58:54 -0300

    feat(fish): sourced homebrew installed apps

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,8 +1,12 @@
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+set -gx EDITOR nvim
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting ""
-    set -gx EDITOR nvim
+    source /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/*
     zoxide init fish | source
+    starship init fish | source
 end
 
 # uv
