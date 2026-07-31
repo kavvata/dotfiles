f0d400f0bfba719e356b52e6f4951299318e0e80
kavvata <gabriel.kawata@gmail.com>
2026-07-31 16:07:53 -0300

    fix(sway): fixes layout notification script

--- private_dot_config/sway/executable_notify-layout-change.sh
+++ private_dot_config/sway/executable_notify-layout-change.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-while true; do
-  swaymsg -t subscribe '["input"]' | while read -r _; do
-    layout=$(swaymsg -t get_inputs | jq -r '.[] | select(.type=="keyboard") | .xkb_active_layout_name' | head -n1)
-    notify-send "Keyboard Layout" "$layout" --expire-time 800
-  done
+swaymsg -t subscribe -m '["input"]' | while read -r _; do
+  layout=$(swaymsg -t get_inputs | jq -r '.[] | select(.type=="keyboard") | .xkb_active_layout_name' | head -n1)
+  [ "$layout" != "$(cat /tmp/sway-layout 2>/dev/null)" ] || continue
+  notify-send "Keyboard Layout" "$layout" --expire-time 800
+  echo "$layout" > /tmp/sway-layout
 done
